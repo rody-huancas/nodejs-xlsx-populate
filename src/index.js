d4b0acf25449dbb9f3411e6aaa89b9bb0de321d7
Rody Huancas <rodyhuancas.04@gmail.com>
2024-11-15 09:30:31 -0500

    Feat: read excel

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import xlsxPopulate from 'xlsx-populate'
 
-// Crear archivos excel
+/********* Crear archivos excel *********/
 /*
 xlsxPopulate.fromBlankAsync().then((workbook) => {
   workbook.sheet(0).cell('A1').value('Hello World')
   return workbook.toFileAsync('./salida.xlsx')
 })
 */
+/*
 async function main() {
   const workbook = await xlsxPopulate.fromBlankAsync()
 
@@ -23,6 +24,26 @@ async function main() {
   workbook.sheet(0).cell('C3').value(21)
 
   workbook.toFileAsync('./excel/salida.xlsx')
+}
+*/
+
+/********* Leer archivos excel *********/
+
+async function main() {
+  const workbook = await xlsxPopulate.fromFileAsync('./excel/salida.xlsx')
+  //   leer por celda
+  //   const value = workbook.sheet('Sheet1').cell('A1').value()
+  //   const value2 = workbook.sheet('Sheet1').cell('A2').value()
+  //   console.log(value)
+  //   console.log(value2)
+
+  //   leer todo
+  //   const value = workbook.sheet('Sheet1').usedRange().value()
+  //   console.log(value)
+
+  // leer por rango
+  const value = workbook.sheet('Sheet1').range('A1:B2').value()
+  console.log(value)
 }
 
 main()
